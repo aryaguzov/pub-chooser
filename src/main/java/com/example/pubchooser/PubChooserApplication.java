eaec4311c0fa2fc82ca135c9f3497a546d4adbe7
aryaguzov <riahuzooleksii@gmail.com>
2022-07-12 18:49:49 +0300

    Initial commit

---
+++ src/main/java/com/example/pubchooser/PubChooserApplication.java
@@ -0,0 +1,13 @@
+package com.example.pubchooser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubChooserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PubChooserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pubchooser/PubChooserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pubchooser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubChooserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
